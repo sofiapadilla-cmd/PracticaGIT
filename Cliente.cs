08d9f2b31569536f04b67ef6d699c2314af1aa03
VanillaSaffii <purpblue139@gmail.com>
2026-05-07 12:27:29 -0700

    test clientes y programa

---
+++ Cliente.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Cliente
+{
+    public void MostrarInfo()
+    {
+        Console.WriteLine("Nombre: Carlos");
+        Console.WriteLine("Correo: carlos.perez@gmail.com");
+    }
+}
